1b3182e03f97c0bc16b061e9473dd9990f64136d
16ziyoda <z.xakimjonova@newuu.uz>
2025-11-12 21:14:50 +0500

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main() {
    
-    int n;
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i){
-      cin >> a[i];
-    } 
+   int n;
+   cin>>n;
+   vector<int> a(n);
+   for (int i = 0; i < n; ++i){
+     cin >> a[i];
+ } 
 
     for (int i = n - 1; i >= 0; --i) {
         cout << a[i]<<" ";
